3cfb2cfc647ba4c09006f4bb47ca030d2e0cc970
mentor <mentor_nila@bitbucket.org>
2019-07-21 10:04:24 +0200

    Prepared users Authentication.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,15 @@
 <?php
-
+use App\User;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'User',
+            'email' => 'user@test.com',
+            'password' => Hash::make('secret')
+        ]);
     }
 }
